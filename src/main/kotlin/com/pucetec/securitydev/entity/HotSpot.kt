b7ba09ed9dd126bb9a69ca79c77fb1febfdee3a3
Julian <aboghacy229@gmail.com>
2026-06-21 11:49:46 -0500

    entity <<HotSpot>> created

---
+++ src/main/kotlin/com/pucetec/securitydev/entity/HotSpot.kt
@@ -0,0 +1,30 @@
+package com.pucetec.securitydev.entity
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToMany
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
+
+//Con Entity indicamos a Kotlin que esto es una tabla en la base de datos.
+@Entity
+@Table(name = "hotspot")
+class HotSpot(
+    @Id
+    //Definimos un Id autoincremental
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+    val latitude: Double=0.0,
+    val longitude: Double=0.0,
+    val modality: String="",
+    val description: String="",
+
+    @ManyToOne
+    @JoinColumn(name = "user_id") // Esta anotación le dice a la base de datos cómo se llamará la columna de unión (FK)
+    val users: Users? = null
+)
